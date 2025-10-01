bc7aff5bc22cc2d2314879c5ef8f151fdb2ad772
PLDQ <767517488@qq.com>
2025-10-01 15:00:22 +0000

    chore: add image-watermark project

---
+++ native/image-watermark/include/watermark.h
@@ -0,0 +1,20 @@
+#ifndef IMAGE_FREETYPE_NODE_WATERMARK_H
+#define IMAGE_FREETYPE_NODE_WATERMARK_H
+
+#ifdef WATERMARKDLL_EXPORTS
+    #define WATERMARK_API __declspec(dllexport)
+#else
+    #define WATERMARK_API __declspec(dllimport)
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //IMAGE_FREETYPE_NODE_WATERMARK_H
---
+++ native/image-watermark/src/main.cpp
@@ -0,0 +1,3 @@
+
+#include "watermark.h"
+
